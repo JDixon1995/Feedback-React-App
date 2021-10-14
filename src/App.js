0284f7ae5b9a5df576657aba9d02a5c36b37a474
John <jddhalogamer@gmail.com>
2021-10-13 20:05:50 -0500

    set state for a rudimentary average system.

--- src/App.js
+++ src/App.js
@@ -11,22 +11,27 @@ const App = () => {
   const [ good, setGood ] = useState(0);
   const [ neutral, setNeutral ] = useState(0);
   const [ bad, setBad ] = useState(0);
-  const [ all, setAll] = useState(0);
+  const [ all, setAll ] = useState(0);
+  const [ rating, setRating ] = useState(0);
 
   const handleGoodClick = () => {
     setGood( good + 1);
     setAll( all + 1)
+    setRating( rating + 1 );
   }
 
   const handleNeutralClick = () => {
     setNeutral( neutral + 1);
     setAll( all + 1);
+    setRating( rating + 0);
   }
 
   const handleBadClick = () => {
     setBad( bad + 1);
     setAll( all + 1);
+    setRating( rating - 1);
   }
+
 
   return (
     <div className="App">
@@ -39,6 +44,8 @@ const App = () => {
       <StatNum text="neutral" counter={neutral} />
       <StatNum text="bad" counter={bad} />
       <StatNum text="all" counter={all} />
+      <StatNum text="average" counter={rating} />
+      <StatNum text="positive" />
     </div>
   );
 }
